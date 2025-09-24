cbbdfc5fb62996e850bf03af99c2856c8f772f1d
Samuel Smith <samsmithsmusic@gmail.com>
2025-09-24 14:16:55 -0600

    Template Engine Unit 1

--- controllers/unit1.js
+++ controllers/unit1.js
@@ -4,9 +4,13 @@ import path from 'path';
 const __filename = fileURLToPath(import.meta.url);
 export const __dirname = path.dirname(__filename);
 
-const home = (req, res) => {res.sendFile(path.join(__dirname, '../src/views/home.html'));};
-const about = (req, res) => {res.sendFile(path.join(__dirname, '../src/views/about.html'));};
-const products = (req, res) => {res.sendFile(path.join(__dirname, '../src/views/products.html'));};
+// const home = (req, res) => {res.sendFile(path.join(__dirname, '../src/views/home.html'));};
+// const about = (req, res) => {res.sendFile(path.join(__dirname, '../src/views/about.html'));};
+// const products = (req, res) => {res.sendFile(path.join(__dirname, '../src/views/products.html'));};
+
+const home = (req, res) => {const title = 'Welcome Home' ; res.render('home',{title});};
+const about = (req, res) => {const title =  'About Me'; res.render('about',{title});};
+const products = (req, res) => {const title = 'Products' ; res.render('products',{title});};
 
 const wife = (req,res) => {res.send("Maddie Smith");};
 
--- server.js
+++ server.js
@@ -7,10 +7,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV || 'production';
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'src/views'));
 app.use('/', routes);
  
 app.listen(PORT, () => {
